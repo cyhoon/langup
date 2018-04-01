8da30f9b39a881f042d3ad3ae39651bb88e150c9
iHello <ihello0720@gmail.com>
2018-04-01 11:48:42 +0900

    UI 수정

--- extension/src/containers/Content.js
+++ extension/src/containers/Content.js
@@ -16,8 +16,13 @@ class Content extends Component {
         return (
             <div className={cx('content')}>
                 <Title word="hello" />
-                <Mean mean="1. (만났을 때의 인사로) 안녕(하세요); 인사; (전화에서나 남의 관심을 끌 때) 여보세요" />
-                <Mean mean="2. 이런, 어머나(놀라움을 나타냄)" />
+                <div className={cx('mean_list')}>
+                    <Mean mean="1. (만났을 때의 인사로) 안녕(하세요); 인사; (전화에서나 남의 관심을 끌 때) 여보세요" />
+                    <Mean mean="2. 이런, 어머나(놀라움을 나타냄)" />
+                    <Mean mean="2. 이런, 어머나(놀라움을 나타냄)" />
+                    <Mean mean="2. 이런, 어머나(놀라움을 나타냄)" />
+                    <Mean mean="2. 이런, 어머나(놀라움을 나타냄)" />
+                </div>
             </div>
         );
     }
