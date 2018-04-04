9e94ba864bdf589e9bb71a3ff37686a195b83db4
iHello <ihello0720@gmail.com>
2018-04-04 10:05:09 +0900

    서버 라우터 구조 적용

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,8 +1,17 @@
+/**
+ * Module dependencies.
+ */
+
 const koaRouter = require('koa-router');
 const router = new koaRouter();
 
+/**
+ * Router dependencies.
+ */
+
 const auth = require('./auth');
 
+// account
 router.use('/auth', auth.routes());
 
 module.exports = router;
