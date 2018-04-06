7c8e86dc59f4ddc1b77b62649d0a37570a040b67
iHello <ihello0720@gmail.com>
2018-04-06 14:44:05 +0900

    인증 미들웨어 구현

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+const tokenHelper = require('../helper/token');
+
+const authMiddleware = async (ctx, next) => {
+  const token = ctx.headers['x-access-token'];
+  let decodeToken = null;
+
+  try {
+    decodeToken = await tokenHelper.verifyToken(token);
+    if (decodeToken.sub !== 'token') throw "token error";
+  } catch (error) { // token expired4
+    ctx.status = 200;
+    ctx.body = {
+      status: 50,
+      message: '토큰 인증 실패',
+    }
+  }
+
+  ctx.token = decodeToken;
+  await next();
+};
+
+module.exports = authMiddleware;
