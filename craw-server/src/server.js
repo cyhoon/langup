02c65f34e135d4d5ba527e802e0583456bbb6944
JeffChoi <ihello0720@gmail.com>
2018-05-11 13:48:18 +0900

    [Chrome extension] append word data

--- craw-server/src/server.js
+++ craw-server/src/server.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const host = 'http://endic.naver.com/searchAssistDict.nhn?query=';
 
 const wordCraw = async (word) => {
-
     await request(host + word, async (err, response, body) => {
         if (err) throw err;
 
@@ -26,22 +25,29 @@ const wordCraw = async (word) => {
             await models.MeanDictionary.create(meanDictionary); // insert mean dictionary
         });
     });
-
 }
 
 (async () => {
 
     console.log('Start data crawling');
 
-    const english_dict = fs.readFileSync('./resource/bad/words.txt', 'utf8').split('\n'); // english file
+    const bad_dict = fs.readFileSync('./resource/bad/words.txt', 'utf8').split('\n'); // english file
+    const adjectives_dict = fs.readFileSync('./resource/adjectives/words.txt', 'utf8').split('\n'); // english file
+    const adverbs_dict = fs.readFileSync('./resource/adverbs/words.txt', 'utf8').split('\n');
+    const conjunctions_dict = fs.readFileSync('./resource/conjunctions/words.txt', 'utf8').split('\n');
+    const nouns_dict = fs.readFileSync('./resource/nouns/words.txt', 'utf8').split('\n');
+    const prepositions_dict = fs.readFileSync('./resource/prepositions/words.txt', 'utf8').split('\n');
+    const pronouns_dict = fs.readFileSync('./resource/pronouns/words.txt', 'utf-8').split('\n');
+    const verbs_dict = fs.readFileSync('./resource/verbs/words.txt', 'utf-8').split('\n');
+    const english_basic_dict = fs.readFileSync('./resource/english_words2.txt', 'utf-8').split('\n');
 
-    await english_dict.forEach(async english_word => { // insert english
+    await english_basic_dict.forEach(async english_word => { // insert english
         /* 단어를 입력할 때 \r이 들어가는것을 방지해야함. */
         engDictionary = { word: english_word.replace(/\r?\n|\r/g, "") };
         await models.EngDictionary.create(engDictionary); // insert eng dictionary
     });
 
-    await english_dict.forEach(async english_word => { // insert mean
+    await english_basic_dict.forEach(async english_word => { // insert mean
         await wordCraw(english_word);
     });
 })();
