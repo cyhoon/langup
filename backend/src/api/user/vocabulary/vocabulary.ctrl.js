f6bf4d206d6221fb994299c46fb8e9a86609c73d
JeffChoi <ihello0720@gmail.com>
2018-05-23 21:08:40 +0900

    사용자 커스텀 단어장 생성
    중요하지 않음 보류함.

--- backend/src/api/user/vocabulary/vocabulary.ctrl.js
+++ backend/src/api/user/vocabulary/vocabulary.ctrl.js
@@ -25,7 +25,7 @@ exports.list = async (ctx, err) => { // 나의 단어장 들 보기
 };
 
 exports.create = async (ctx, err) => { // 나의 단어장 생성
-
+  ctx.body = '사용자 가 단어를 생성 안할 것 같아서 보류';
 }
 
 exports.modifyName = async (ctx, err) => { // 나의 단어장 이름 수정
